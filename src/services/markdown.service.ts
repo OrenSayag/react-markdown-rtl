ebc787af4b895c28f49ace6eaccc2827417456d5
Oren Sayag <oren.s@techmarketing.co.il>
2024-03-14 17:27:10 +0200

    v1.1 ✨ - added basic colors

--- src/services/markdown.service.ts
+++ src/services/markdown.service.ts
@@ -9,6 +9,10 @@ export const mdToHtml = (md: string): string => {
       .replace(/--(.*)--/gim, "<div dir='ltr' style='text-align: left'>$1</div>") // LTR
       .replace(/~~(.*)~~/gim, "<span style='text-decoration-line: line-through'>$1</span>") // Strike-through
       .replace(/_(.*)_/gim, "<span style='text-decoration-line: underline;'>$1</span>") // Underline
+      .replace(/{RED}(.*){RED}/gim, "<span style='color: red;'>$1</span>") // Color - Red
+      .replace(/{BLUE}(.*){BLUE}/gim, "<span style='color: blue;'>$1</span>") // Color - Blue
+      .replace(/{GREEN}(.*){GREEN}/gim, "<span style='color: green;'>$1</span>") // Color - Green
+      .replace(/{YELLOW}(.*){YELLOW}/gim, "<span style='color: yellow;'>$1</span>") // Color - Yellow
       .replace(/\n(.*)/gim, "<br />$1"); // new line
   return toHTML.trim(); // using trim method to remove whitespace
 };
